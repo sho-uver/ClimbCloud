8e4f9f5597bb369b5b523f0aeeb8461303df8ec4
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-06-03 23:45:02 +0900

    雲のぼりゲーム

---
+++ Assets/ClearDirectort.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClearDirectort : MonoBehaviour {
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown (0)) { SceneManager.LoadScene ("GameScene"); }
+    }
+}
